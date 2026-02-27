8105ee477755ed3a6102819da5f80ddc81ac4ac1
RashmiHG90 <rashmi.hguk@gmail.com>
2026-02-27 13:29:29 +0000

    Added deleting task from added task using filter

--- context-api-task-app/src/DisplayTask.jsx
+++ context-api-task-app/src/DisplayTask.jsx
@@ -6,11 +6,23 @@ function DisplayTask(){
 
     let contextRef = useContext(TaskContext);
 
+    let handleDelete=(index)=>{
+        console.log("Calling delete function on index:"+ index)
+        //filter returns new array with filter condition
+        //Other way of doing it..
+        //let newtaskdata = contextRef.Tasks.filter((_,i)=>i!=index)
+        //contextRef.setTasks(newtaskdata);
+        contextRef.setTasks(contextRef.Tasks.filter((_,i)=>i!=index));
+    }
+
     return(
         <>
-        <ol>
-            {contextRef.Tasks.map((task,index)=><li key={index}>{task}</li>)}
-        </ol>
+        <ul>
+            {contextRef.Tasks.map((task,index)=><li key={index}>{task}
+            <input type="button" value="Delete" onClick={()=>handleDelete(index)} />
+            </li>)}
+            
+        </ul>
         </>
     )
 }
