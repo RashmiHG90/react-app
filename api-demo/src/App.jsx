4903b1bc5906105f9232b0c99951fbeb0fc3683f
RashmiHG90 <rashmi.hguk@gmail.com>
2026-03-03 12:09:04 +0000

    Used axios to get data from api and display using both es5 and es6 style

--- api-demo/src/App.jsx
+++ api-demo/src/App.jsx
@@ -1,18 +1,49 @@
 import React, { useState, useEffect } from 'react'; 
 import './App.css'; 
+import axios from 'axios';
 
 function App() { 
 
 const [data, setData] = useState(null); 
 
-async function fetchData() { 
-const response = await fetch('https://jsonplaceholder.typicode.com/todos/1'); 
-const jsonData = await response.json(); 
-setData(jsonData); 
+async function fetchDataES6Style() { 
+  try{
+     const response = await fetch('https://jsonplaceholder.typicode.com/todos/1'); 
+     const jsonData = await response.json(); 
+     setData(jsonData); 
+  }
+  catch(error){
+    console.log(error);
+  }
+
 } 
 
+function fetchDataES5Style(){
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response=>response.json()).then(jsonData=>setData(jsonData))
+  .catch(error=>console.log(error));
+}
+
+function loadDataUsingAxiosES5Style(){
+ axios.get('https://jsonplaceholder.typicode.com/todos/1')
+ .then(response=>setData(response.data)).catch(error=>console.log(error));
+}
+
+async function loadDataUsingAxiosES6Style(){
+  try{
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+    setData(response.data) 
+  }catch(error){
+    console.log(error);
+  }
+
+}
+
 useEffect(() => { 
-fetchData(); 
+//fetchDataES6Style(); 
+//fetchDataES5Style();
+//loadDataUsingAxiosES5Style();
+loadDataUsingAxiosES6Style();
 }, []); 
 
 return ( 
