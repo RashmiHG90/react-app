4d262686023c6e0a79a3da0ca51d77a78fab4854
RashmiHG90 <rashmi.hguk@gmail.com>
2026-07-04 13:24:08 +0100

    context with state management for managing theme, skeleton structure

---
+++ context-api-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { ThemeProvider } from './context/ThemeProvider'
+import ContentComponent from './components/ContentComponent'
+
+function App() {
+
+
+  return (
+    <ThemeProvider>
+      <ContentComponent></ContentComponent>
+    </ThemeProvider>
+
+  )
+}
+
+export default App
---
+++ context-api-app/src/components/ContentComponent.jsx
@@ -0,0 +1,14 @@
+import { useContext } from 'react';
+import ThemeContext from '../context/ThemeContext';
+
+
+export default function ContentComponent() {
+
+    const { theme, toggleTheme } = useContext(ThemeContext)
+    return (
+        <div>
+            Current Theme {theme}
+            <button onClick={toggleTheme}>Toggle Theme</button>
+        </div>
+    )
+}
