4074782f388f093d13773327e10718dfea2cccb9
RashmiHG90 <rashmi.hguk@gmail.com>
2026-07-08 07:19:23 +0100

    commiting book details with routing to different components

--- book-library/src/components/Books.jsx
+++ book-library/src/components/Books.jsx
@@ -1,17 +1,27 @@
 import { useContext } from 'react'
+import { useLocation } from 'react-router-dom';
 import { BooksContext } from '../BooksContext'
 
 
 export default function Books() {
 
+    const query = new URLSearchParams(useLocation().search);
+    const searchItem = query.get('search') || '';
     const books = useContext(BooksContext);
+
+    const filteredBooks = searchItem
+        ? books.filter((book) =>
+            book.title.toLowerCase().includes(searchItem.toLowerCase()) ||
+            book.author.toLowerCase().includes(searchItem.toLowerCase())
+        )
+        : books;
 
     return (
         <div>
             <h1>All Books</h1>
             <ul>
                 {
-                    books.map((book) => (
+                    filteredBooks.map((book) => (
                         <li key={book.id}>{book.title} by {book.author}</li>
                     ))
                 }
